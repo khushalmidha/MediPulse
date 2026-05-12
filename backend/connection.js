3cac4752b309ccf820f5dbc60a67287b453b6e20
khushalmidha <khushalmidha24@gmail.com>
2026-05-13 03:42:18 +0530

    fail fast when mongodb is not connected

--- backend/connection.js
+++ backend/connection.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 const connectMongo = async (url) => {
-	mongoose
-		.connect(url)
-		.then(() => console.log("Connected to mongoDB"))
-		.catch((err) => console.log("Mongo Error: ", err));
+	if (!url) {
+		throw new Error("DATABASE_URL is not configured");
+	}
+
+	await mongoose.connect(url, {
+		serverSelectionTimeoutMS: 10000,
+	});
+	console.log("Connected to mongoDB");
 };
 
 export default connectMongo;
