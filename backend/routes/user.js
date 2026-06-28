019e085c3bbc99c399a26b1637ab4b23ada75731
Khushal Midha <khushalmidha24@gmail.com>
2026-06-28 11:23:14 +0530

    chore: clean unused package artifacts

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,7 +9,6 @@ import {
 import userValidation from "../middleware/validateUser.js";
 import {
 	deleteUserById,
-	getAllUsers,
 	getUserById,
     updateUserData,
 } from "../controller/user.js";
@@ -21,10 +20,7 @@ userRouter.post("/signup", userSignup);
 userRouter.post("/google-auth", googleAuth);
 userRouter.post("/forgot-password/send-otp", sendPasswordResetOtp);
 userRouter.post("/forgot-password/reset", resetPasswordWithOtp);
-
-// userRouter.get("/:id", userValidation, getUserById);
 userRouter.delete("/:id", userValidation, deleteUserById);
-// userRouter.get("/", userValidation, getAllUsers);
 userRouter.get("/", userValidation, getUserById);
 userRouter.put("/", userValidation, updateUserData);
 
